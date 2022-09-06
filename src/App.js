a500081b8f263784c8bf46d1ce9bc49f8ce42d5a
Ana Smith <anasmith@Mac-Pro.local>
2022-09-06 16:49:18 -0400

    componente general de la app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='Screen'> 
+      hola, estoy centradito
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
