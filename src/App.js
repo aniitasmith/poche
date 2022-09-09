4e0e2b75e9427d8a08beed3705a1f964bcb60632
aniitasmith <aniitasmith1992@gmail.com>
2022-09-09 15:27:30 -0400

    ajustes en el componente HOME

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Header from './componentes/Header/Header.js';
-import Footer from './componentes/Footer/Footer.js';
+import Home from './componentes/Home/Home.js';
 
 function App() {
   return (
     <div className='app'>
-      <div className='screen'>
-        <Header></Header>
-        <Footer></Footer>
-      </div>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/componentes/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const Home = () => (
+<div className='home'>
+        <Header></Header>
+        <Footer></Footer>
+</div>
+);
+
+export default Home;
+
+
