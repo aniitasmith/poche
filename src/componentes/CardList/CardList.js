a46a79a9d715c554b385fd9d28938b5461b69532
aniitasmith <aniitasmith1992@gmail.com>
2022-10-05 12:55:11 -0400

    crear sub listas

--- src/componentes/CardList/CardList.js
+++ src/componentes/CardList/CardList.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ratings from '../../ratings.json';
 import Card from "../card/card";
+import '../CardList/cardList.css';
 
 function CardList(props) {
-
-  const cardMap = ratings.map( 
+  const filterRatings = ratings.filter( 
+    (local) => local.ratings[0].category.name === props.category)
+  const cardMap = filterRatings.map( 
     (local) => <Card local={local}></Card>
-  );
+  ); 
 
-  return ( <> {cardMap} </> )
+  return ( 
+   <>
+      <div className="top-list">
+        <h1 className="title-list"> Top {props.category}</h1>
+        <a className="all-list" href="">View all</a>
+      </div> 
+      {cardMap} 
+
+  </>)
 }
 
 export default CardList;
--- src/componentes/Home/Home.js
+++ src/componentes/Home/Home.js
@@ -3,12 +3,17 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import CardList from '../CardList/CardList';
 import SearchBar from '../SearchBar/SearchBar';
+import '../Home/Home.css';
 
 const Home = () => (
 <div className='home'>
   <Header></Header>
   <SearchBar></SearchBar>
-  <CardList></CardList>
+  <div className='content'>
+    <CardList category="breakfast"></CardList>
+    <CardList category="lunch"></CardList>
+    <CardList category="dinner"></CardList>
+  </div>
   <Footer></Footer>
 </div>
 );
